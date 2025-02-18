c1b01700cb77ac541234e609e4bf0395bcbd9295
Alexey Yumanov Laptop <inactive0073@gmail.com>
2025-02-18 15:15:02 +0400

    Добавлен учебный пример rabbitmq

--- app/rabbitmq/consumer.py
+++ app/rabbitmq/consumer.py
@@ -1,47 +1,55 @@
 import asyncio
+import json
+import time
 
 import aiormq
 from aiormq.abc import DeliveredMessage
 
-# from app.config_data.config import load_config
 
-# callback, вызываемый на каждое сообщение для консьюмера
+# Callback-функция, вызываемая на каждое сообщение для консьюмера
 async def on_message(message: DeliveredMessage):
-    print(message.body.decode())
-    
-    # Явное подтверждение получения и обработки ошибки
+    # Декодируем и десериализуем сообщение
+    body: dict = json.loads(message.body.decode('utf-8'))
+
+    print(f"Сообщение {body.get('counter')} получено")
+    print(f"{body.get('text')} {body.get('counter')}")
+
+    # Имитируем выполнение тяжелой CPU-bound задачи
+    time.sleep(body.get('delay'))
+
+    # Явно подтверждаем получение и обработку сообщения
     await message.channel.basic_ack(delivery_tag=message.delivery.delivery_tag)
-    
+    print(f"Сообщение {body.get('counter')} обработано")
+
 
 async def main():
-    # Подключение к RabbitMQ
-    # url = load_config().rabbitmq.URL
-    # connection = await aiormq.connect(url)
-    connection = await aiormq.connect("amqp://rabbitmqlogin:rabbitmqpassword@localhost/")
-    
-    
-    # Создание канала
+    # Подключаемся к RabbitMQ
+    connection = await aiormq.connect("amqp://bot_shop_admin:Gfhjkm0073@localhost/")
+
+    # Создаем канал
     channel = await connection.channel()
-    
-    # Объявление точки обмена 
+
+    # Объявляем точку обмена (создается, если не существует)
     await channel.exchange_declare("test_exchange", exchange_type="direct")
-    
-    # Объявление очереди (создается, если не существует)
-    declare_ok = await channel.queue_declare("test_done")
-    
-    # Привязка очереди к точке обмена
+
+    # Объявляем очередь (создается, если не существует)
+    declare_ok = await channel.queue_declare('test_queue')
+
+    # Привязываем очередь к точке обмена
     await channel.queue_bind(
         queue=declare_ok.queue,
         exchange="test_exchange",
         routing_key="test_routing_key"
     )
-    
-    # Определение количества сообщений, которые консьюмер может получить за один раз
+
+    # Определяем количество сообщений, которое консьюмер может получить за один раз
     await channel.basic_qos(prefetch_count=1)
-    
-    # Настройка прослушивания очереди
-    await channel.basic_consume(declare_ok.queue, on_message)
-    
+
+    # Настраиваем прослушивание очереди
+    await channel.basic_consume(declare_ok.queue, on_message, no_ack=False)
+
+    # Создаем бесконечный цикл для ожидания сообщений для консьюмера из очереди
     await asyncio.Future()
-    
+
+
 asyncio.run(main())
--- app/rabbitmq/producer.py
+++ app/rabbitmq/producer.py
@@ -1,31 +1,47 @@
 import asyncio
 import aiormq
+import random
+import json
 
-# from app.config_data.config import load_config
 
+async def publish():
+    # Подключаемся к RabbitMQ
+    connection = await aiormq.connect("amqp://bot_shop_admin:Gfhjkm0073@localhost/")
 
-async def producer():
-    # url = load_config().rabbitmq.URL
-    # Подключение к RabbitMQ
-    # connection: AbstractConnection = await aiormq.connect(url)
-    connection = await aiormq.connect("amqp://rabbitmqlogin:rabbitmqpassword@localhost/")
-
-    
-    # Создание канала
+    # Создаем канал
     channel = await connection.channel()
-    
-    # Объявление точки обмена (создается, если не существует)
-    await channel.exchange_declare("test_exchange", exchange_type="direct")
-    
-    # Отправка сообщения в exchange
-    await channel.basic_publish(
-        body="Привет из RabbitMQ!".encode("utf-8"),
-        exchange="test_exchange",
-        routing_key="test_routing_key"
-    )
-    
-    # Закрытие соединения
-    await connection.close()
-    
 
-asyncio.run(producer())
+    # Объявляем точку обмена (создается, если не существует)
+    await channel.exchange_declare("test_exchange", exchange_type="direct")
+
+    # Инициализируем переменную для счетчика сообщений
+    counter = 1
+
+    # Запускаем бесконечный цикл публикации сообщений в RabbitMQ
+    while True:
+
+        # Добавляем случайную задержку перед отправкой очередного сообщения
+        await asyncio.sleep(random.randrange(1, 4))
+
+        # Создаем словарь, из которого будет сформировано тело сообщения
+        body = {
+            'delay': 1 if not counter % 2 else 5,
+            'text': 'Привет из RabbitMQ №',
+            'counter': counter
+        }
+
+        # Отправляем сообщение в exchange
+        await channel.basic_publish(
+            body=json.dumps(body).encode('utf-8'),
+            exchange="test_exchange",
+            routing_key="test_routing_key"
+        )
+
+        # Выводим информационное сообщение в консоль
+        print(f'Сообщение № {counter} опубликовано!')
+
+        # Увеличиваем счетчик сообщений
+        counter += 1
+
+
+asyncio.run(publish())

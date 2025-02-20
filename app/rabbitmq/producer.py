5f3b8f72d30d6ab027d9299e97102ce3e07ff790
Alexey Yumanov Laptop <inactive0073@gmail.com>
2025-02-20 15:27:07 +0400

    Добавлен учебный пример prod/consumer

--- app/rabbitmq/producer.py
+++ app/rabbitmq/producer.py
@@ -1,47 +1,44 @@
 import asyncio
-import aiormq
-import random
 import json
+from datetime import datetime, timedelta, timezone
+
+import aiormq
 
 
-async def publish():
+async def publish_message():
     # Подключаемся к RabbitMQ
-    connection = await aiormq.connect("amqp://bot_shop_admin:Gfhjkm0073@localhost/")
+    connection = await aiormq.connect('amqp://rabbitmqlogin:rabbitmqpassword@localhost/')
 
     # Создаем канал
     channel = await connection.channel()
 
     # Объявляем точку обмена (создается, если не существует)
-    await channel.exchange_declare("test_exchange", exchange_type="direct")
+    await channel.exchange_declare('main_exchange', exchange_type='direct')
 
-    # Инициализируем переменную для счетчика сообщений
-    counter = 1
+    # Определяем время, когда сообщение должно быть обработано
+    scheduled_time = (datetime.now(timezone.utc) + timedelta(seconds=5)).isoformat()
 
-    # Запускаем бесконечный цикл публикации сообщений в RabbitMQ
-    while True:
+    # Создаем словарь, из которого будет сформировано тело сообщения
+    body = {
+        'text': 'Привет из RabbitMQ!',
+    }
 
-        # Добавляем случайную задержку перед отправкой очередного сообщения
-        await asyncio.sleep(random.randrange(1, 4))
-
-        # Создаем словарь, из которого будет сформировано тело сообщения
-        body = {
-            'delay': 1 if not counter % 2 else 5,
-            'text': 'Привет из RabbitMQ №',
-            'counter': counter
-        }
-
-        # Отправляем сообщение в exchange
-        await channel.basic_publish(
-            body=json.dumps(body).encode('utf-8'),
-            exchange="test_exchange",
-            routing_key="test_routing_key"
+    # Отправляем сообщение в `main_exchange`
+    await channel.basic_publish(
+        body=json.dumps(body).encode('utf-8'),
+        exchange='main_exchange',
+        routing_key='main_routing_key',
+        properties=aiormq.spec.Basic.Properties(
+            headers={
+                'scheduled_time': scheduled_time
+            }
         )
+    )
 
-        # Выводим информационное сообщение в консоль
-        print(f'Сообщение № {counter} опубликовано!')
+    print(f'Published message with scheduled time: {scheduled_time}')
 
-        # Увеличиваем счетчик сообщений
-        counter += 1
+    # Закрываем соединение
+    await connection.close()
 
 
-asyncio.run(publish())
+asyncio.run(publish_message())

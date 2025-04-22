6c350918c63742bdacf2183edfe463058fb3cf34
Alexey Yumanov <inactive0073@gmail.com>
2025-04-22 22:18:04 +0400

    fix commands, added customer dialog

---
+++ app/dialogs/customer_user/dialogs.py
@@ -0,0 +1,22 @@
+from aiogram import F
+from aiogram.types import ContentType
+from aiogram_dialog import Dialog, Window, ShowMode
+from aiogram_dialog.widgets.text import Format, Case, List
+from aiogram_dialog.widgets.kbd import (
+    Group,
+    SwitchTo,
+    Toggle,
+    Button,
+    Multiselect,
+    Row,
+    Start,
+)
+from aiogram_dialog.widgets.input import TextInput, MessageInput
+from app.states.customer.start import StartCustomerSG
+
+customer_dialog = Dialog(
+    Window(
+        state=StartCustomerSG.start
+    ),
+    getter=...,
+)

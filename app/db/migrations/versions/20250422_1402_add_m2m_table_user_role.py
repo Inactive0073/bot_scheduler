bd848c2188158603d5a2e53b4abe33e265b790f0
Alexey Yumanov Laptop <inactive0073@gmail.com>
2025-04-23 09:44:29 +0400

    fix migrations

--- app/db/migrations/versions/20250422_1402_add_m2m_table_user_role.py
+++ app/db/migrations/versions/20250422_1402_add_m2m_table_user_role.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'd19a98cdfc2e'
-down_revision: Union[str, None] = 'c2423b2e97fc'
+down_revision: Union[str, None] = '5d8402fda7f0'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
